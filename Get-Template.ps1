5fc817568fa8aefe201fa956e54f135a5ac80e72
pcruemlang <44089327+pcruemlang@users.noreply.github.com>
2018-10-15 09:13:09 +0200

    Create Get-Template.ps1

---
+++ Get-Template.ps1
@@ -0,0 +1,49 @@
+function get-template
+{
+<#
+.DESCRIPTION 
+
+this script will set the given user a new password 
+and send $whoever an email with the text template filled up and  ready to forward  
+
+.EXAMPLE
+
+get-newjoinertemplate -user $whoever 	
+
+#>
+[cmdletbinding()]
+param( 
+[parameter(mandatory=$true)]
+[string]$user
+)
+Write-Warning "Attention this script will reset the users password ! " -WarningAction Inquire 
+$abfrage = get-aduser $user -Properties * 
+$data = @{}
+$data.add("aName",$abfrage.Name)
+$data.add("Name",$abfrage.GivenName)
+$data.add("unumber",$abfrage.SamAccountName)
+$data.add("pw",("{0:1}" -f ($abfrage.physicalDeliveryOfficeName+$abfrage.SamAccountName.substring(1,5))))
+$data.add("email",$abfrage.EmailAddress)
+$data.add("ext",$abfrage.telephoneNumber)
+$data.add("Manager",$abfrage.Manager)
+
+write-warning "password will be now set to $($data.pw) for $($data.Name)" -WarningAction Inquire 
+Set-ADAccountPassword $user -Reset -NewPassword (ConvertTo-SecureString $($data.pw) -AsPlainText -Force ) 
+$body = @"
+Dear [$($data.Name)]
+
+ manager : $($data.Manager) 
+
+
+Personnel information :
+login : 	[$($data.unumber)]
+Password = 	[$($data.pw)]
+Email : 		[$($data.email)]
+Initials : 	              [$($data.unumber)]
+Tel. Ext. : 	[$($data.ext)]
+
+"@
+
+Send-MailMessage -From "$whoever@smtp.com" -to "$whoever@smtp.com" -Subject "your text $($data.aName)"  -body $body  -SmtpServer "exchange.com"
+}
+
