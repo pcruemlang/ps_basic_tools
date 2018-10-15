be2a542ab36907ebcc8b3357cb75b85af34a7f19
pcruemlang <44089327+pcruemlang@users.noreply.github.com>
2018-10-15 08:52:20 +0200

    Update get-usergroups.ps1

--- get-usergroups.ps1
+++ get-usergroups.ps1
@@ -5,8 +5,9 @@ param(
 [parameter(mandatory=$true)]
 [string]$user
 )
+$path = read-host "Enter the Path"
 $start_time = get-date
  Get-ADPrincipalGroupMembership $user  | select name,GroupScope,GroupCategory | 
-sort-object -property name | export-csv -NoTypeInformation -encoding utf8 -path C:\Scripts\Zurich_Helpdesk\$user.csv
+sort-object -property name | export-csv -NoTypeInformation -encoding utf8 -path $path\$user.csv
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).milliSeconds) millisecond(s)"
 }
