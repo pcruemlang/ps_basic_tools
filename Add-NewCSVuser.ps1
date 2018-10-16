c04147970922a8d54fb2562e2d90cfff0995184c
pcruemlang <44089327+pcruemlang@users.noreply.github.com>
2018-10-16 15:55:29 +0200

    Create Add-NewCSVuser.ps1

---
+++ Add-NewCSVuser.ps1
@@ -0,0 +1,17 @@
+# creates user with a pre formatted CSV file 
+# works with powershell PSVersion  2.0
+# from goateepfe 
+$p = import-csv "C:\Scripts\Zurich_Helpdesk\users.csv" -Delimiter ";"
+$p | select-object Title, Department,City, state, office, EmployeeID,`
+@{n='name';e={($_.'First Name'.substring(0,3)+$_.'last Name').substring(0,7)}}, `
+@{n='samaccountname';e={($_.'First Name'.substring(0,3)+$_.'last Name').substring(0,6)}},`
+@{n='Displayname';e={($_.'First Name'+' , '+$_.'last Name')}},`
+@{n='GivenName';e={($_.'First Name')}},`
+@{n='SurName';e={($_.'Last Name')}},`
+@{n='HomeDirectory';e={('\\homedrive$\'+$_.'EmployeeID')}},`
+@{n='Description';e={($_.'last name' +' - '+$_.'EmployeeID'+' Dept.: '+$_.'department')}}, `
+@{n='Path';e={('OU='+$_.'City'+','+(Get-ADRootDSE).defaultNamingContext)}} | 
+New-AdUser `
+-changepasswordatlogon $true `
+-enabled $true  `
+-accountpassword $(convertTo-securestring $pwd -asplaintext -force ) # not secure change method 
