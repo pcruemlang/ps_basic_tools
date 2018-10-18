585861b0727301b5fbcdf64a24b15871a01656df
pcruemlang <44089327+pcruemlang@users.noreply.github.com>
2018-10-18 09:06:43 +0200

    Update compare-groups.ps1

--- compare-groups.ps1
+++ compare-groups.ps1
@@ -9,5 +9,8 @@ $date = get-date -UFormat %d%m%y
 remove-item a1.txt 
 remove-item b1.txt 
 write-host "Comparing members from $groupa to $groupb  .." 
-Compare-Object $a $b -IncludeEqual | ft inputobject, @{n="file";e={if($_.SideIndicator -eq '=>') { "$GroupA" } if ($_.SideIdincator -eq '<=') {"$GroupB"} else { "Found in Both groups" } }} | Out-File -encoding utf8 $GroupA_compared_$GroupB_$date.txt 
+Compare-Object $a $b -IncludeEqual |
+ft inputobject,`
+@{n="file";e={if($_.SideIndicator -eq '=>') { "$GroupA" } if ($_.SideIdincator -eq '<=') {"$GroupB"} else { "Found in Both groups" } }} |
+Out-File -encoding utf8 $GroupA_compared_$GroupB_$date.txt 
 }
