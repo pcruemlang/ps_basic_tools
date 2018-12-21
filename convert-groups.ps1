985547d3b0b5406261a45850cbe33a6f472e7c71
pcruemlang <44089327+pcruemlang@users.noreply.github.com>
2018-12-21 13:04:25 +0100

    Update convert-groups.ps1
    added some comments

--- convert-groups.ps1
+++ convert-groups.ps1
@@ -15,12 +15,12 @@ $userList |
 foreach-object {
 write-host  "Adding : "  -foregroundcolor green 
 $_.Name +" - " +$_.samaccountname + "  to " + $grouptoadd
-# add-ADGroupMember $grouptoadd -member $_.samaccountname 
-Start-Sleep –Seconds 1
+# add-ADGroupMember $grouptoadd -member $_.samaccountname # uncomment to make it work 
+Start-Sleep –Seconds 1 # this is just for cosmetics 
 write-host  "Removing : " -foregroundcolor red  
 $_.Name +" - " + $_.samaccountname  + " from " + $grouptoremove 
-#Remove-ADGroupMember $grouptoremove -member $_.samaccountname 
-Start-Sleep –Seconds 1
+#Remove-ADGroupMember $grouptoremove -member $_.samaccountname  # uncomment to make it work 
+Start-Sleep –Seconds 1 # this is just for cosmetics 
  }#foreach
  
  }#function
